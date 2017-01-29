4a851c255510d52f4b3a3cace9f8c4dfb8dc3297
Luqman <luqmanrom@gmail.com>
2017-01-29 14:31:57 +0800

    WIP: Game Star

---
+++ src/Game.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <StarFrame />
+        <ButtonFrame />
+        <AnswerFrame />
+      </div>     
+    );
+  }
+}
+
+class StarFrame extends Component {
+	render() {
+		return (
+			<div> 
+				...
+			</div>
+		);
+
+	}
+}
+
+class AnswerFrame extends Component {
+	render() {
+		return (
+			<div> 
+				...
+			</div>
+		);
+
+	}
+}
+
+class ButtonFrame extends Component {
+	render() {
+		return (
+			<div> 
+				...
+			</div>
+		);
+
+	}
+}
+
+export default App;
