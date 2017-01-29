8e5fe474e4674a593b4c48aa7e02cfb57169da2b
Luqman <luqmanrom@gmail.com>
2017-01-29 12:44:04 +0800

    Add jQuery and modify App component

---
+++ src/Card.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+class Card extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    //this.state = {name: 'Luqman'}
+
+  }
+
+  componentDidMount() {
+    let component = this;
+    $.get('https://api.github.com/users/' + this.props.user, function(data) {
+      component.setState(data);
+    })
+    
+  }
+  render() {
+    return (
+        <div>
+          <h1>Hello , {this.state.name}</h1>
+        </div>
+    );
+  }
+}
+
+export default Card;
